32f471d9be0837ba73d8d97e3d6fb8f4f60077ad
Gnomy17 <gnomy@ce.sharif.edu>
2019-09-07 14:42:02 +0430

    server progress

--- src/server/main.go
+++ src/server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"math"
 	"net/http"
 	"os"
 )
 
 type miniServer int
+
+var requestCounter = 11
 
 type mainServer int
 
@@ -21,7 +24,20 @@ func (m mainServer) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	//cut file into chunks here then make miniServers for each chunk
+	const chunkSize = 1 << 25
+	fileInfo, _ := file.Stat()
+	var fileSize int64 = fileInfo.Size()
+	numOfParts := int(math.Ceil(float64(fileSize) / float64(chunkSize)))
+	_, _ = res.Write([]byte("port:" + string(requestCounter*100) + "parts:" + string(numOfParts)))
 
+	for i := 0; i < numOfParts; i++ {
+		var newMiniServer miniServer
+		go newMiniServer.init(fileName, i)
+	}
+	requestCounter++
+	if requestCounter == 80 {
+		requestCounter = 11
+	}
 }
 
 func main() {
